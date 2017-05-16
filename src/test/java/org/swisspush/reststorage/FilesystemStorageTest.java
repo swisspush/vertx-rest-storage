eb89e6e40b4374571307a888c028a20850bbda9a
Marc-André Weber <maweber84@gmail.com>
2017-05-16 08:23:05 +0200

    More tests for FilesystemStorage

--- src/test/java/org/swisspush/reststorage/FilesystemStorageTest.java
+++ src/test/java/org/swisspush/reststorage/FilesystemStorageTest.java
@@ -9,9 +9,22 @@ import org.junit.runner.RunWith;
 
 import static com.jayway.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
 
 @RunWith(VertxUnitRunner.class)
 public class FilesystemStorageTest extends FilesystemStorageTestCase {
+
+    @Test
+    public void testGetCollection(TestContext testContext) throws InterruptedException {
+        Async async = testContext.async();
+        String path = TEST_FILES_PATH + "/collection/sub/resources/";
+        with().body("<h1>nemo.html</h1>").put(path + "nemo.html");
+        with().body("<h1>index.html</h1>").put(path + "index.html");
+        get(path).then().assertThat().statusCode(200)
+                .contentType("application/json")
+                .body("resources", hasItems("index.html", "nemo.html"));
+        async.complete();
+    }
 
     @Test
     public void testDeleteCollectionWithRecursiveParameter(TestContext testContext) throws InterruptedException {
