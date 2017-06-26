a9eae9b36271eb3845e206b3ca8f36f7e7c3f1e7
Marc-André Weber <maweber84@gmail.com>
2017-06-26 11:18:05 +0200

    Reverted RestStorageRunner to default configuration

--- src/main/java/org/swisspush/reststorage/RestStorageRunner.java
+++ src/main/java/org/swisspush/reststorage/RestStorageRunner.java
@@ -1,10 +1,7 @@
 package org.swisspush.reststorage;
 
-import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
-import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.LoggerFactory;
-import org.swisspush.reststorage.util.ModuleConfiguration;
 
 /**
  * Created by florian kammermann on 23.05.2016.
@@ -15,15 +12,7 @@ import org.swisspush.reststorage.util.ModuleConfiguration;
 public class RestStorageRunner {
 
     public static void main(String[] args) {
-
-        JsonObject mainStorageConfig = ModuleConfiguration.with()
-                .storageType(ModuleConfiguration.StorageType.redis)
-                .build()
-                .asJsonObject();
-
-        DeploymentOptions storageOptions = new DeploymentOptions().setConfig(mainStorageConfig).setInstances(4);
-
-        Vertx.vertx().deployVerticle("org.swisspush.reststorage.RestStorageMod", storageOptions, event -> {
+        Vertx.vertx().deployVerticle("org.swisspush.reststorage.RestStorageMod", event -> {
             LoggerFactory.getLogger(RestStorageMod.class).info("rest-storage started");
         });
     }
